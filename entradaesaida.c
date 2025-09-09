b7936a60bc191e430f1ffeb679a1b5a9845ab2aa
GILSON <gilson.gss2020@gmail.com>
2025-09-09 20:54:48 -0300

    Update entradaesaida.c

--- entradaesaida.c
+++ entradaesaida.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main () {
+
+    int idade = 25;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3;
+    char letra = 'A';
+    char nome[20] = "sergio";
+
+
+printf("A idade do %s é: %d\n", nome, idade);
+printf("A altura é: %e\n",altura);
+printf("A opção é: %c\n", opcao);
+
+
+
+
+/*
+prntf("%fornato1 %formato2 %formato3" , variavel1, variavel, variavel3)
+
+%d: imprime um inteiro no formato decimal
+%i: Equivalente a %d.
+%f: imprime um numero de ponto flutuante no formato padrão.
+%e: imprime um numero de ponto flutuante na notação cientifica
+%c: imprime um único caractere.
+%s: imprime uma cadeia (string) de caracteres.
+*/
+
+
+}
